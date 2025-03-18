e6d3812674636389e3064f608721326feefa027b
oggnimodd <arnold.ds.dev@gmail.com>
2025-03-18 04:22:44 +0000

    feat: Implement line number customization in CodeMirror editor

--- src/lib/components/editor/CodeMirror.svelte
+++ src/lib/components/editor/CodeMirror.svelte
@@ -1,8 +1,55 @@
 <script lang="ts">
   import CodeMirror from "svelte-codemirror-editor";
   import { javascript } from "@codemirror/lang-javascript";
+  import { editorStore } from "$lib/stores/editor.svelte";
+  import { EditorView, lineNumbers as cmLineNumbers } from "@codemirror/view";
+  import { Compartment } from "@codemirror/state";
 
   let value = $state("");
+  let view: EditorView | null = $state(null);
+
+  const lineNumbersCompartment = new Compartment();
+
+  let baseExtensions = $derived.by(() => {
+    return [
+      lineNumbersCompartment.of(
+        editorStore.lineNumbers
+          ? cmLineNumbers({
+              formatNumber: (n: number) =>
+                String(n + editorStore.lineNumberStart - 1),
+            })
+          : EditorView.theme({ ".cm-gutters": { display: "none" } })
+      ),
+    ];
+  });
+
+  $effect(() => {
+    if (view) {
+      view.dispatch({
+        effects: lineNumbersCompartment.reconfigure(
+          editorStore.lineNumbers
+            ? cmLineNumbers({
+                formatNumber: (n: number) =>
+                  String(n + editorStore.lineNumberStart - 1),
+              })
+            : EditorView.theme({ ".cm-gutters": { display: "none" } })
+        ),
+      });
+    }
+  });
+
+  $effect(() => {
+    if (view) {
+      console.log(view);
+    }
+  });
 </script>
 
-<CodeMirror bind:value lang={javascript()} />
+<CodeMirror
+  on:ready={(e) => {
+    view = e.detail;
+  }}
+  bind:value
+  lang={javascript()}
+  extensions={baseExtensions}
+/>
